d43437799fd79fa45809f3f96f0aa7f7cf2031da
NathalyMR <nathaly.mena1@correo.icesi.edu.co>
2020-09-25 15:55:24 -0500

    Pantalla 1

--- src/logic.js
+++ src/logic.js
@@ -3,13 +3,20 @@ constructor(app){
 this.app=app;
 this.app.createCanvas(1700,935)
 this.fondo=this.app.loadImage("src/Images/fondo.jpg")
-
-
+this.inicio=this.app.loadImage("src/Images/Pantalla 1.jpg")
+this.pantalla=0;
+this.font=this.app.loadFont("src/Font/MoonbrightDemo-1GGn2.ttf")
 
 
 } 
 draw(){
-    this.app.image(this.fondo,0,0)
+   this.app.textFont(this.font)
+    switch(this.pantalla){
+      case 0: 
+      this.app.image(this.inicio,0,0)
+      this.app.text("Leer",770,670)
+      this.app.textSize(110)
+    }
     
 }
 
